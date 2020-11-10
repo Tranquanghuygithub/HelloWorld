ad1795b7f0232a95ce4d6042dd549c4b274e69b9
Tran Huy <tranhuy26031998@gmail.com>
2020-11-10 16:23:05 +0700

    Ghi de commit truoc do

---
+++ src/HelloWorld/Hello.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("tran quang huy");
+        for(int i=0;i<1000;i++){
+            int n=i;
+            System.out.println(n);
+        }
+    }
+
+}
